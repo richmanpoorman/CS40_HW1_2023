e6d1eb093c981cdf9d85fbb213f77cfbdff589bc
Helena Simson <68454497+ArkfallBravo@users.noreply.github.com>
2023-09-10 13:56:32 -0400

    Updated makefile with new .h files and created linePackage files

---
+++ filesofpix/linePackage.h
@@ -0,0 +1,10 @@
+/*
+ *      linePackage.h
+ *      By Helena Simson, Matthew Wong, 9/9/2023
+ *      hw1: filesofpix
+ *
+ *      Summary:
+ *      
+ *      TODO: write summary for linePackage.h
+ *      
+ */
