afb2467fcdab1f12f25e492cab8a8806c05faa2e
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2023-09-11 13:21:41 -0400

    extended ascii test to utf-8

--- testmaker.py
+++ testmaker.py
@@ -1,13 +1,11 @@
 
 
-with open("testFile4.txt", "w", encoding = "utf-8") as file:
+with open("linesOfAll.txt", "w", encoding = "utf-8") as file:
     line : str = ""
-    for j in range(400):
-        for i in range(400):
-            charASCII = i % 128
-            if (charASCII == ord("\n") or (charASCII >= ord("0") and charASCII <= ord("9"))):
-                continue
-            line += chr(charASCII)
-        line += "\n"
-        file.write(line)
+    for i in range(255):
+        charASCII = i % 128
+        if (charASCII == ord("\n") or (charASCII >= ord("0") and charASCII <= ord("9"))):
+            continue
+        line += chr(charASCII)
+    file.write(line)
 
