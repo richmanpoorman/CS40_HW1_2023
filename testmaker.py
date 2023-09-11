3e969837465cf8257e77e061df45faa7d54d6123
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2023-09-11 12:07:17 -0400

    tests

--- testmaker.py
+++ testmaker.py
@@ -1,10 +1,13 @@
 
-file = open("testFile.txt", "w")
 
-line : str = ""
+with open("testFile4.txt", "w", encoding = "utf-8") as file:
+    line : str = ""
+    for j in range(400):
+        for i in range(400):
+            charASCII = i % 128
+            if (charASCII == ord("\n") or (charASCII >= ord("0") and charASCII <= ord("9"))):
+                continue
+            line += chr(charASCII)
+        line += "\n"
+        file.write(line)
 
-for i in range(1000):
-    line += 'a'
-
-file.write(line)
-file.close()
