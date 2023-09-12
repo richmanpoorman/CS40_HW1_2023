bf7ea04241b236f23293ccb244ae851518e8bb18
Helena Simson <68454497+ArkfallBravo@users.noreply.github.com>
2023-09-12 19:31:26 -0400

    updated test.c

--- filesofpix/test.c
+++ filesofpix/test.c
@@ -7,6 +7,7 @@
 #include "reader.h"             // TODO: check if we need these .h files
 #include <seq.h>
 #include <mem.h>
+#include "cleaner.h"
 
 // size_t readaline(FILE *inputfd, char **datapp); //TODO :: Remove this
 
@@ -36,36 +37,48 @@ void test(FILE *inputFile)
 
         // (void) testReader;
 
-        // tests
-        printf("running tests!\n");
-        char *str = ALLOC(3);
-        str[0] = 'a';
-        str[1] = 'b';
-        str[2] = 'c';
-        // char *ptr = "abc";
-        LinePackage test = LinePackage_new(str, 3);
-        size_t testLength = LinePackage_size(test);
-        printf("testLength is %li\n", testLength);
-        char *testByteList = LinePackage_byteList(test);
-        for (size_t i = 0; i < testLength; i++) {
-                printf("%c", testByteList[i]);
+        // // tests
+        // printf("running tests!\n");
+        // char *str = ALLOC(3);
+        // str[0] = 'a';
+        // str[1] = 'b';
+        // str[2] = 'c';
+        // // char *ptr = "abc";
+        // LinePackage test = LinePackage_new(str, 3);
+        // size_t testLength = LinePackage_size(test);
+        // printf("testLength is %li\n", testLength);
+        // char *testByteList = LinePackage_byteList(test);
+        // for (size_t i = 0; i < testLength; i++) {
+        //         printf("%c", testByteList[i]);
+        // }
+        // printf("\n");
+
+        // printf("%c\n", str[1]);
+
+        // str[1] = 'x';
+
+        // LinePackage_set_size(test, 2);
+        // testLength = LinePackage_size(test);
+        // printf("testLength is %li\n", testLength);
+        // testByteList = LinePackage_byteList(test);
+        // for (size_t i = 0; i < testLength; i++) {
+        //         printf("%c", testByteList[i]);
+        // }
+        // printf("\n");
+
+        // LinePackage_free(test);
+
+        // test cleaner
+        Seq_T testReader = reader(inputFile);
+        Seq_T testCleaner = cleaner(testReader);
+        while (Seq_length(testCleaner) > 0) {
+                LinePackage testPackage = Seq_remlo(testCleaner);
+                for (size_t i = 0; i < LinePackage_size(testPackage); i++) {
+                        printf("%i ", LinePackage_byteList(testPackage)[i]);
+                }
+                printf("\n");
+                LinePackage_free(testPackage);
         }
-        printf("\n");
-
-        printf("%c\n", str[1]);
-
-        str[1] = 'x';
-
-        LinePackage_set_size(test, 2);
-        testLength = LinePackage_size(test);
-        printf("testLength is %li\n", testLength);
-        testByteList = LinePackage_byteList(test);
-        for (size_t i = 0; i < testLength; i++) {
-                printf("%c", testByteList[i]);
-        }
-        printf("\n");
-
-        LinePackage_free(test);
 
         (void) inputFile;
 }
