0b48c9b18e95e1fada5b16d5beadd0b4f47969fb
Helena Simson <68454497+ArkfallBravo@users.noreply.github.com>
2023-09-17 13:17:59 -0400

    wrote function descriptions, fixed formatting

--- filesofpix/stringToBytes.c
+++ filesofpix/stringToBytes.c
@@ -34,12 +34,12 @@ LinePackage convertSingleLine(LinePackage line);
  *
  * Converts data of digit bytes to character bytes
  * Inputs:
- *      LinePackage line: the line to convert
- * Return: a linepackage with the converted line
+ *      Seq_T cleanedLines: the lines to convert to character bytes
+ * Return: The converted lines
  * Expects
- *      line to be a valid linepackage. TODO: improve this
+ *      cleanedLines to be now uncorrupted with only digit and space ASCII characters TODO: Check this
  * Notes:
- *      will always end line WITHOUT \n character
+ *      TODO: Finish notes
  *
  ************************/
 Seq_T stringToBytes(Seq_T cleanedLines)
@@ -67,17 +67,17 @@ LinePackage convertSingleLine(LinePackage line)
         for (size_t readHead = 0; readHead < size; readHead++) {
                 char byte = byteList[readHead];
                 
-                if (byte >= '0' && byte <= '9') { // If the value if a digit, then accumulate
+                if (byte >= '0' && byte <= '9') { /* If the value if a digit, then accumulate */ /* TODO: WTF does this comment mean? */
                         hasPushed = false;
                         value *= 10;
                         value += byte - '0';   
                 }
-                else { // Otherwise, put it into the string
+                else { /* Otherwise, put it into the string */
                         hasPushed = true;
                         byteList[writeHead] = (char) value;
                         writeHead++;
                         value = 0;
-                        if (byte == '\n') { // If it is an endline, put the endline on as well
+                        if (byte == '\n') { /* If it is an endline, put the endline on as well */
                                 byteList[writeHead] = '\n';
                                 writeHead++;
                         }
@@ -87,7 +87,7 @@ LinePackage convertSingleLine(LinePackage line)
                 byteList[writeHead] = (char) value;
                 writeHead++;
         }
-        // writeHead always points to one after the last written place, which is the size
+        /* writeHead always points to one after the last written place, which is the size */
         LinePackage_set_size(line, writeHead);
 
         return line;
